02e7a0c39e6fc3e3b671146ac2a93f29f74638c8
KeithRussel <keithmaniacop@gmail.com>
2022-07-23 20:21:08 +0800

    Rename of folder and Add 07-AuthorizationForm

---
+++ JavascriptnReact/07-authorizationForm/contact.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// const e = React.createElement;
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: "swordfish",
+      authorized: false,
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector('input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth,
+    });
+  }
+
+  render() {
+    const login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input type="password" placeholder="Password" />
+        <input type="submit" />
+      </form>
+    );
+
+    const contactInfo = (
+      <ul>
+        <li>client@example.com</li>
+        <li>555.555.5555</li>
+      </ul>
+    );
+    return (
+      <div id="authorization">
+        <h1>{this.state.authorized ? "Contact" : "Enter the Password"}</h1>
+        {this.state.authorized ? contactInfo : login}
+      </div>
+    );
+  }
+}
+
+// const domContainer = document.querySelector("#app");
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(e(<Contact />));
+ReactDOM.render(<Contact />, document.getElementById("app"));
