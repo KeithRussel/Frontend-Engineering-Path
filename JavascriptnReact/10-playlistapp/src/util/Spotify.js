0f6f4e8cea437ff238fa811f56c579139cce6464
KeithRussel <keithmaniacop@gmail.com>
2022-07-28 12:53:22 +0800

    Fix getAccessToken method in Spotify.js

--- JavascriptnReact/10-playlistapp/src/util/Spotify.js
+++ JavascriptnReact/10-playlistapp/src/util/Spotify.js
@@ -1,27 +1,29 @@
-import React from "react";
-
-const accessToken = "a659494b83f74e7289950de6c356e4ba";
+const clientId = "a659494b83f74e7289950de6c356e4ba";
+const redirectURI = "http://localhost:3000/";
+let accessToken;
 const Spotify = {
   getAccessToken() {
     if (accessToken) {
       return accessToken;
-    } else if(!accessToken) {
-        // Check the URL to see if it has a Spotify access token in it.
-        // Use window.location.href and the .method to retrieve the access token and expiration time from the URL.
-        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/&expires_in=([^&]*)/);
+    } else {
+      var accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+      var expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    }
 
-        // If the access token and expiration time are in the URL, implement the following steps:
-        // 1. Set the access token value
-        // 2. Set a variable for expiration time
-        // 3 Set the access token to expire at the value for expiration time.
-        // 4. Clear the parameters from the URL, so the app doesn't try grabbing the access token after it expires.
-        if (accessTokenMatch) {
-            window.setTimeout(() => accessToken = '', expiresIn * 1000);
-            window.history.pushState('Access Token', null, '/');
-            return accessTokenMatch[1];
-        }
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    } else {
+      // The access token variable is empty and is not in the URL
+      const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+
+      window.location = accessURL;
     }
   },
+  searchtermInput(term) {},
 };
 
 export default Spotify;
