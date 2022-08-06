d13171481173804cb571303f355a888cf2a71117
KeithRussel <keithmaniacop@gmail.com>
2022-08-06 14:45:30 +0800

    Push Passing Thoughts proj

---
+++ JavascriptnReact/11-passingthoughts/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { AddThoughtForm } from "./components/AddThoughtForm";
+import { Thought } from "./components/Thought";
+import { generateId, getNewExpirationTime } from "./utils/utilities";
+import "./App.css";
+
+function App() {
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: "This is a place for your passing thoughts.",
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
+  const addThought = (thought) => {
+    setThoughts((thoughts) => [thought, ...thoughts]);
+  };
+
+  const removeThought = (thoughtIdToRemove) => {
+    setThoughts((thoughts) =>
+      thoughts.filter((thought) => thought.id !== thoughtIdToRemove)
+    );
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Passing Thoughts</h1>
+      </header>
+      <main>
+        <AddThoughtForm addThought={addThought} />
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought
+              key={thought.id}
+              thought={thought}
+              removeThought={removeThought}
+            />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
